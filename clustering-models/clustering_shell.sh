5b7ffbfcf6dd8415b1c21dedac3d7c37d235a2ea
Himani2000 <negi.himani11@gmail.com>
2020-07-21 19:00:55 +0530

    updated clustering shell

--- clustering-models/clustering_shell.sh
+++ clustering-models/clustering_shell.sh
@@ -2,13 +2,24 @@
 
 echo "Running all the clustering models"
 
-echo "Running for the density based clustering algorithms"
+#echo "Running for the density based clustering algorithms"
 
-python densityclustering.py ideology_crop_image_dataset Image_features_crop Image_results_crop
+#python densityclustering.py ideology_image_dataset image_features_15july image_results_15july
 
 
-echo "Running for the hierarchical clustering algorithms"
-python hierarchicalclustering.py ideology_crop_image_dataset Image_features_crop Image_results_crop
+#echo "Running for the hierarchical clustering algorithms"
+#python hierarchicalclustering.py ideology_image_dataset image_features_15july image_results_15july
+
+
+#echo "Running for the density -crop"
+#python densityclustering.py ideology_crop_image_dataset image_features_15july_crop image_results_15july_crop
+
+
+#echo "Running for the hierarchical clustering algorithms -- crop "
+#python hierarchicalclustering.py ideology_crop_image_dataset image_features_15july_crop image_results_15july_crop
 
 echo "Running for generating the text and the pdf files with filter"
-python image_results_utils.py ideology_crop_image_dataset Image_results_crop/ Image_results_crop_text_pdf yes
+python image_results_utils.py ideology_image_dataset image_results_15july/ image_results_15july_text_pdf4 yes
+
+echo "Running for generating the text and the pdf files with filter-- crop "
+python image_results_utils.py ideology_crop_image_dataset image_results_15july_crop/ image_results_15july_crop_text_pdf4 yes
